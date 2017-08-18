ea41acc021bddae6ffc3a2ebf8a0d2a7ade95028
Dakota Walsh <kotawalsh@gmail.com>
2017-08-18 05:56:12 -0400

    set data file to print

--- kota/kota.py
+++ kota/kota.py
@@ -2,7 +2,7 @@
 #/home/kota/cloud/dots/scripts/alias_helper_data/alias
 
 # Open the config file
-f = open('/home/kota/git/tools/kota/kota.py', 'r')
+f = open('/home/kota/git/tools/kota/data/alias', 'r')
 
 line = f.read()
 f.close()
