e4f410717ef903d67cd85cef91d742936260555c
Dakota Walsh <kotawalsh@gmail.com>
2017-08-18 05:51:08 -0400

    add kota alias printer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# tools
+# Tools
 Tiny misc scripts and tools I made for myself
+
+* kota - Print a file with my alias's
---
+++ kota/kota.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#/home/kota/cloud/dots/scripts/alias_helper_data/alias
+
+# Open the config file
+f = open('/home/kota/git/tools/kota/kota.py', 'r')
+
+line = f.read()
+f.close()
+
+print(line)
