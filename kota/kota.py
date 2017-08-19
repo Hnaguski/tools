68c59e4bf3ae55e41ea77ec29274d560a85cd9a7
Dakota Walsh <kotawalsh@gmail.com>
2017-08-19 17:23:13 -0400

    get user path so this works on other computers

--- kota/kota.py
+++ kota/kota.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 #/home/kota/cloud/dots/scripts/alias_helper_data/alias
 
+import os
+
 # Open the config file
-f = open('/home/kota/git/tools/kota/data/alias', 'r')
+f = open(os.path.expanduser('/home/kota/git/tools/kota/data/alias'), 'r')
 
 line = f.read()
 f.close()
