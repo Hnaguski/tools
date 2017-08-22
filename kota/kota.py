97ee15f634ba3dbd4c1bfc713525c897a3756b15
Dakota Walsh <kotawalsh@gmail.com>
2017-08-22 04:26:55 -0400

    make kota slightly less of a shitty hack

--- kota/kota.py
+++ kota/kota.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
-#/home/kota/cloud/dots/scripts/alias_helper_data/alias
+
+#----------------------------------#
+# ,--. ,--.         ,--.           #
+# |  .'   / ,---. ,-'  '-. ,--,--. #
+# |  .   ' | .-. |'-.  .-'' ,-.  | #
+# |  |\   \' '-' '  |  |  \ '-'  | #
+# `--' '--' `---'   `--'   `--`--' #
+#            kotajacob.tk          #
+# Copyright (C) 2017  Dakota Walsh #
+#----------------------------------#
 
 import os
 
-# Open the config file
-f = open(os.path.expanduser('/home/kota/git/tools/kota/data/alias'), 'r')
+# Open the config file - this is where I keep my alias's
+f = open(os.path.expanduser('~/.zshrc'), 'r')
 
-line = f.read()
+raw_file = f.readlines()
+for line in raw_file:
+    if line[:5] == "alias":
+        print (line[:len(line)-1])
+
 f.close()
-
-print(line)
